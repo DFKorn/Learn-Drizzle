b96bb8485a0d3c593f41896998916c8c6d7412dd
DFKorn <d1man2192@mail.ru>
2025-03-02 17:02:38 +0300

    add tables and change schemas

--- src/drizzle/schema.ts
+++ src/drizzle/schema.ts
@@ -1,9 +1,29 @@
-import { integer, pgTable, varchar, uuid} from 'drizzle-orm/pg-core'
+import { integer, pgTable, varchar, uuid, pgEnum, index, uniqueIndex, unique, boolean} from 'drizzle-orm/pg-core'
+
+export const UserRole = pgEnum('userRole',['ADMIN','BASIC'])
 
 
 export const usersTable = pgTable("users", {
     id: uuid('id').primaryKey().defaultRandom(),  //integer().primaryKey().generatedAlwaysAsIdentity(),
     name: varchar('name',{ length: 255 }).notNull(),
-    // age: integer().notNull(),
-    // email: varchar({ length: 255 }).notNull().unique(),
+    age: integer('age').notNull(),
+    email: varchar('email',{ length: 255 }).notNull(),
+    role: UserRole('userRole').default('BASIC').notNull()
+  }, table => {
+    //  !!!! depricated !!!
+    // return {
+    //   emailIndex: uniqueIndex('emailIndex').on(table.email),
+    //   uniqueNameAndAge: unique('uniqueNameAndAge').on(table.name, table.age)
+    // }
+    return [
+      uniqueIndex('emailIndex').on(table.email),
+      unique('uniqueNameAndAge').on(table.name, table.age)
+    ]
   });
+
+  export const UserPreferencesTable = pgTable('userPreferences', {
+    id: uuid('id').primaryKey().defaultRandom(),
+    emailUpdates: boolean('emailUpdates').notNull().default(false),
+    userId: uuid('userId').references(() => usersTable.id).notNull(),
+  })
+  
