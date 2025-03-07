e571c98cd6f17a81a5693f884d693aa7180f25ec
DFKorn <d1man2192@mail.ru>
2025-03-07 21:08:06 +0300

    finish SQL Style Data selection

--- src/main.ts
+++ src/main.ts
@@ -23,7 +23,7 @@
 
 // setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
 
-import { asc, desc, sql } from 'drizzle-orm'
+import { asc, count, desc, eq, gt, sql } from 'drizzle-orm'
 import {db} from './drizzle/db'
 import { UserPreferencesTable, usersTable } from './drizzle/schema'
 
@@ -65,47 +65,84 @@ import { UserPreferencesTable, usersTable } from './drizzle/schema'
 
 
 // --------- Query Style Data selection ----------------    
+// async function main(){
+//   // await db.insert(UserPreferencesTable).values({
+//   //   emailUpdates: true,
+//   //   userId: '67ae394e-0b5f-4622-bac7-295173c09a02'
+//   // })
+//    const users = await db.query.usersTable.findMany({
+//       columns: {age: true, id: true, name: true},
+
+//       // ---- "with" examples -----
+//       //with: {preferences: true}
+
+//       // with: {
+//       //   posts: {with: {postCategories: true}}
+//       // }
+
+//       // with: {preferences: {
+//       //   columns: {
+//       //     emailUpdates: true
+//       //   }
+//       // }}
+
+//       //----- "oderBy" object examples ----
+//       //orderBy: desc(usersTable.age)
+//       //orderBy: asc(usersTable.age)
+
+//       //----- "orderBy" function examples ----
+//       orderBy: (table, funcs) => funcs.asc(table.age),
+//       // orderBy: (table, {asc}) => asc(table.age) // more concise 
+
+
+//       //----- "where" function examples ----
+
+//       //where: (table, funcs) => funcs.eq(table.age, 25)
+//       where: (table, funcs) => funcs.between(table.age, 20, 29)
+
+
+//       //offset: 0
+//     })
+
+//     console.log(users)
+//   }
+
+// --------- SQL Style Data selection ----------------
 async function main(){
   // await db.insert(UserPreferencesTable).values({
   //   emailUpdates: true,
   //   userId: '67ae394e-0b5f-4622-bac7-295173c09a02'
   // })
-   const users = await db.query.usersTable.findMany({
-      columns: {age: true, id: true, name: true},
-
-      // ---- "with" examples -----
-      //with: {preferences: true}
-
-      // with: {
-      //   posts: {with: {postCategories: true}}
-      // }
-
-      // with: {preferences: {
-      //   columns: {
-      //     emailUpdates: true
-      //   }
-      // }}
-
-      //----- "oderBy" object examples ----
-      //orderBy: desc(usersTable.age)
-      //orderBy: asc(usersTable.age)
-
-      //----- "orderBy" function examples ----
-      orderBy: (table, funcs) => funcs.asc(table.age),
-      // orderBy: (table, {asc}) => asc(table.age) // more concise 
 
 
-      //----- "where" function examples ----
-
-      //where: (table, funcs) => funcs.eq(table.age, 25)
-      where: (table, funcs) => funcs.between(table.age, 20, 29)
-
-
-      //offset: 0
+   /* const users = await db
+    .select({
+      id: usersTable.id, 
+      age: usersTable.age,
+      emailUpdates: UserPreferencesTable.emailUpdates
     })
+    .from(usersTable)
+    .where(eq(usersTable.age, 25))
+    .leftJoin(UserPreferencesTable, eq(UserPreferencesTable.userId, usersTable.id))
+ */
+
+
+    // ---- groupBy selection -----
+    const users = await db
+    .select({
+      name: usersTable.name,
+      count: count(usersTable.name),
+    })
+    .from(usersTable)
+    .groupBy(usersTable.name)
+    .having(columns => gt(columns.count, 0))
+
+
 
     console.log(users)
   }
+
+
 
   
 
